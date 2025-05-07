98c10011f98fae3f050c4c7c05b254fafffe05d4
JD-edu <96219601+JD-edu@users.noreply.github.com>
2025-05-07 15:16:35 +0900

    Remove build install folder

--- build/jdamr200_cartographer/CMakeFiles/jdamr200_cartographer_uninstall.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for jdamr200_cartographer_uninstall.
-# This may be replaced when dependencies are built.
--- build/jdamr200_teleop/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jd/jdamr_200_ros/install/jdamr200_teleop'
